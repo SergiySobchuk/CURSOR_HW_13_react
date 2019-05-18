b5b6f4edcba8c84f8d306e941edaf7ee1da954d2
SergiySobchuk <Sergiy.Sobchuk@gmail.com>
2019-05-18 20:59:59 +0300

    Updates

---
+++ precache-manifest.ef2bb561cc979a0857978f59832cf6ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77578134522745499c2544c68b4eefb",
+    "url": "/CURSOR_HW_13_react/index.html"
+  },
+  {
+    "revision": "5896d29d5be43f1575eb",
+    "url": "/CURSOR_HW_13_react/static/js/2.4cace690.chunk.js"
+  },
+  {
+    "revision": "ae4c206a361af233d2c4",
+    "url": "/CURSOR_HW_13_react/static/js/main.98817cee.chunk.js"
+  },
+  {
+    "revision": "30201126a5e10dfd355b",
+    "url": "/CURSOR_HW_13_react/static/js/runtime~main.da2ceae1.js"
+  }
+]);
